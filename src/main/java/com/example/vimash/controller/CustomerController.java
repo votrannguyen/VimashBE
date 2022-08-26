b976e81a77350fabbe2773ed7c8e0198b246da67
Phien <phientran2000@gmail.com>
2022-08-26 09:56:29 +0700

    ang

--- src/main/java/com/example/vimash/controller/CustomerController.java
+++ src/main/java/com/example/vimash/controller/CustomerController.java
@@ -1,4 +1,19 @@
 package com.example.vimash.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.vimash.bean.jpa.ResultBean;
+
+@RestController
 public class CustomerController {
+	
+	
+	@GetMapping("/customer")
+	public ResponseEntity<ResultBean> getCustomer(){
+		int a =1;
+		int b =2;
+		return null;
+	}
 }
