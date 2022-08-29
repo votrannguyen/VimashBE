920499ba8fe8654e6b868145ac25b1c2bec44bbf
buiducdong <dongbuiduc620@gmail.com>
2022-08-29 10:16:20 +0700

    update get list customer

--- src/main/java/com/example/vimash/services/impl/CustomerServiceImpl.java
+++ src/main/java/com/example/vimash/services/impl/CustomerServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.vimash.services.impl;
 
 import com.example.vimash.bean.jpa.ResultBean;
 import com.example.vimash.bean.jpa.jpa.CustomerEntity;
+import com.example.vimash.bean.jpa.jpa.response.CustomerResponse;
 import com.example.vimash.bean.jpa.jpa.response.customer.CustomerIPageResponse;
 import com.example.vimash.bean.jpa.request.CustomerSearchListRequest;
 import com.example.vimash.dao.CustomerDao;
@@ -15,7 +16,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @Transactional(rollbackFor = { ApiValidateException.class, Exception.class })
@@ -29,8 +33,14 @@ public class CustomerServiceImpl implements CustomerService {
 
         List<CustomerEntity> customerEntityList = null;
         customerEntityList = customerDao.getAllCustomer(name.trim(),code1.trim(), code2.trim(), page, size);
+        List<CustomerResponse> customerResponsesList = customerEntityList.stream().map(customerEntity -> {
+            return new CustomerResponse(customerEntity.getCustomerId(), customerEntity.getCustomerCode(),
+                    customerEntity.getPostCode(), customerEntity.getCustomerName(), customerEntity.getPicName(),
+                    customerEntity.getAddress1(), customerEntity.getPhoneNumber(), customerEntity.getFaxNumber(),
+                    customerEntity.getLeadTime(), customerEntity.getRouteCode(), customerEntity.getCourseCode());
+        }).collect(Collectors.toList());
 
-        return new ResultBean(customerEntityList, Constants.STATUS_OK, Constants.MESSAGE_OK);
+        return new ResultBean(customerResponsesList, Constants.STATUS_OK, Constants.MESSAGE_OK);
     }
 
 //    @Override
